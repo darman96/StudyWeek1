79f74d5578597e97cb0ae7b48b1aacdbda61ce5d
Erik Simon <simonerik1996@gmail.com>
2016-01-04 19:10:39 +0100

    PlaneCharacter
    ~ Updated PlaneCharacter Script
    Flying works now ( sort of :)  )

--- StudyWeek1/Assets/Scripts/PlaneCharacter.cs
+++ StudyWeek1/Assets/Scripts/PlaneCharacter.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class PlaneCharacter : MonoBehaviour {
 
@@ -11,26 +12,47 @@ public class PlaneCharacter : MonoBehaviour {
     public int MaxHP = 5;
     public int Lives = 3;
 
+    // Contains all projectiles for the Weapon Stages
+    public List<GameObject> Projectiles = new List<GameObject>();
+
     private int WeaponMod;
     private int CurrentHP;
     private float CurrentShieldDuration  = 0;
+
+    private float xVelocity = 0;
+    private float zVelocity = 0;
+
+    private Rigidbody rig;
+    private AudioSource audio;
 
 
 	// Use this for initialization
 	void Start ()
     {
         CurrentHP = MaxHP;
+
+        rig = GetComponent<Rigidbody>();
+        audio = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-	
+        // Keep Character on flight level
+	    if(transform.position.y < FlightLevel)
+        {
+            Vector3 curPos = transform.position;
+            curPos.y = FlightLevel;
+            transform.position = curPos;
+        }
+
+        InputHandling();
+
 	}
 
     void FixedUpdate()
     {
-
+        Movement();
     }
 
     void LateUpdate()
@@ -40,17 +62,40 @@ public class PlaneCharacter : MonoBehaviour {
 
     private void InputHandling()
     {
+        // Get velocity
+        xVelocity = Input.GetAxis("Horizontal") * MoveSpeed * Time.deltaTime;
+        zVelocity = Input.GetAxis("Vertical") * MoveSpeed * Time.deltaTime;
 
+        // Shooting
+        if(Input.GetKey(KeyCode.Space))
+        {
+            Attack();
+        }
     }
 
     private void Movement()
     {
+        // Tilt the plane when flying left or right
+        if(xVelocity == 0)
+        {
+            transform.rotation = Quaternion.Euler(-90, 0, 0);
+        }
+        if(xVelocity < 0)
+        {
+            transform.rotation = Quaternion.Euler(-90, 0, -45);
+        }
+        if(xVelocity > 0)
+        {
+            transform.rotation = Quaternion.Euler(-90, 0, 45);
+        }
 
+        transform.Translate(transform.TransformDirection(new Vector3(xVelocity, 0, zVelocity)));
     }
 
     private void Attack()
     {
-
+        // Instantiate projectile of the current weapon stage
+        Instantiate(Projectiles[WeaponMod],transform.position , transform.rotation);
     }
 
     private void CollectPowerUp(int powerUp)
