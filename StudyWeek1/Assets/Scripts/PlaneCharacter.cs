6d0b9a8acb01f01acd5d797f1d68906d254cfe59
Erik Simon <simonerik1996@gmail.com>
2016-01-04 16:51:11 +0100

    PlaneCharacter
    +PlaneCharacter Script

---
+++ StudyWeek1/Assets/Scripts/PlaneCharacter.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaneCharacter : MonoBehaviour {
+
+    public float MoveSpeed = 10f;
+    public float FlightLevel = 10f;
+
+    public int ShieldDuration = 5;
+
+    public int MaxHP = 5;
+    public int Lives = 3;
+
+    private int WeaponMod;
+    private int CurrentHP;
+    private float CurrentShieldDuration  = 0;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        CurrentHP = MaxHP;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    void FixedUpdate()
+    {
+
+    }
+
+    void LateUpdate()
+    {
+
+    }
+
+    private void InputHandling()
+    {
+
+    }
+
+    private void Movement()
+    {
+
+    }
+
+    private void Attack()
+    {
+
+    }
+
+    private void CollectPowerUp(int powerUp)
+    {
+
+        switch(powerUp)
+        {
+            case 0:
+                if (CurrentHP < MaxHP)
+                    CurrentHP++;
+                break;
+
+            case 1:
+                CurrentShieldDuration = ShieldDuration;
+                break;
+
+            case 2:
+                WeaponMod++;
+                break;
+        }
+
+    }
+
+}
