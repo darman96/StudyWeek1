2fdf5243fadf1f593b0b3d9142789092a388ebb7
MasterP9 <philipp@bonus-finanzen.de>
2016-01-04 16:54:07 +0100

    Extra Skripts & Folder
    +Spawn Skript
    +Rotor Rotation
    +EnemySkript Folder
    +UtilitySkript Folder

---
+++ StudyWeek1/Assets/Skripts/UtilitySkripts/RotorRotation.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotorRotation : MonoBehaviour {
+
+    public float m_rotorSpeed = 55;
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(Vector3.right * m_rotorSpeed);
+	}
+}
