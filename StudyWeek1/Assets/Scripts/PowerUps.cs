73b70d23f4f791e126a23bbb379b070b9c21afcc
EchnatonOne <acabral@hotmail.de>
2016-01-04 16:48:48 +0100

    PowerUps
    +PowerUp Script

---
+++ StudyWeek1/Assets/Scripts/PowerUps.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerUps : MonoBehaviour
+{
+    public int destroyPickupAfterXSeconds = 0;
+    public float rotationSpeed = 100f;
+
+    public int buffType = 0;
+
+	// Use this for initialization
+	void FixedUpdate ()
+    {
+        transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter (Collider col)
+    {
+	if (col.gameObject.tag == "Player")
+        {
+//            col.GetComponent<PlaneCharacter>().CollectPowerUp(buffType);
+        }
+
+        Destroy(gameObject, destroyPickupAfterXSeconds);
+	}
+}
