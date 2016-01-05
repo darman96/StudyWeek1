cb6deedea1037f385e22aa84f3f4629d41377a54
Erik Simon <simonerik1996@gmail.com>
2016-01-05 18:18:37 +0100

    Water Assets and Projectile movement imporved
    + Water assets
    ~ Projectile Movement

--- StudyWeek1/Assets/Scripts/Weapons/Projectile.cs
+++ StudyWeek1/Assets/Scripts/Weapons/Projectile.cs
@@ -6,15 +6,24 @@ public class Projectile : MonoBehaviour {
     public float Speed = 100f;
     public bool Explosive = false;
 
+    public float lifetime = 3f;
+
+    private Rigidbody rig;
+
 	// Use this for initialization
 	void Start () {
-	
+        rig = GetComponent<Rigidbody>();
+        rig.AddForce(Vector3.forward * Speed, ForceMode.Impulse);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-        transform.Translate(Vector3.forward * Speed * Time.deltaTime);
-	}
+
+    void Update()
+    {
+        lifetime -= Time.deltaTime;
+        if(lifetime <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
 
     void OnCollisionEnter(Collision col)
     {
@@ -22,6 +31,6 @@ public class Projectile : MonoBehaviour {
         //TODO: Hit detection and Damage
         //TODO: Particle Effect impact / explosion
 
-        Destroy(gameObject);
+        //Destroy(gameObject);
     }
 }
