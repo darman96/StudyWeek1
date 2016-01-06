372b90b712b748bd48b423324c31faac60629195
Erik Simon <simonerik1996@gmail.com>
2016-01-06 18:06:02 +0100

    Spawning and Master Script
    - SpawnEnemy script
    + SpawnPoint script
    + Master Script
    + Wave script

---
+++ StudyWeek1/Assets/Scripts/Wave/Wave.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Wave : MonoBehaviour {
+
+    public List<GameObject> SpawnPoints = new List<GameObject>();
+
+    private bool current = false;
+
+    private SpawnPoint currentSpawnPoint;
+    private int currentSpawnPointIndex = 0;
+
+	// Use this for initialization
+	void Start () {
+        currentSpawnPoint = SpawnPoints[currentSpawnPointIndex].GetComponent<SpawnPoint>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Spawn()
+    {
+        current = true;
+    }
+
+    public bool Finished()
+    {
+
+
+    }
+
+}
