a6fcb4cc7f4ba130b5ee102f29138f13dec34da1
Erik Simon <simonerik1996@gmail.com>
2016-01-06 03:26:16 +0100

    Rotate Ocean
    + RotateOcean Script

---
+++ StudyWeek1/Assets/Scripts/UtilityScripts/RotateOcean.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateOcean : MonoBehaviour {
+
+    public float rotationSpeed = 2f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(new Vector3(0, rotationSpeed * Time.deltaTime, 0));
+	}
+}
